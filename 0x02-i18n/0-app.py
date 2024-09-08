f885b014fc2ae06d0c986fbc08be9452edbb15c1
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-09-08 17:15:32 +0000

    Create basic Flask app with an index route and an HTML template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Flask app module that creates a simple route and renders an HTML template.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+        """
+    Index route that renders the 'index.html' template.
+
+    Returns:
+        str: Rendered HTML page content.
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
